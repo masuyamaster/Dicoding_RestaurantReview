16d521c4a6183c8ca7d0d75ddde42ce11b8a9b82
masuyamaster <roziq@masuya.id>
2021-12-01 20:26:47 +0700

    Codelab Mengambil Data Review Restoran

---
+++ app/src/main/java/com/roziqrizal/restaurantreview/ApiService.kt
@@ -0,0 +1,10 @@
+import com.roziqrizal.restaurantreview.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+}
